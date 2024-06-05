2d05a08533f1ef1c2c0ba93a4bb0998e83b655ae
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-05 12:33:17 -0300

    create test config file

---
+++ src/main/java/com/example/library/config/TestConfig.java
@@ -0,0 +1,15 @@
+package com.example.library.config;
+
+import com.example.library.models.User;
+import com.example.library.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+public class TestConfig  {
+
+
+}
