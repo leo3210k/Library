e76a1bc3f65c58cdd8340a9cb2cd7e14d6bb5c30
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-05 12:35:22 -0300

    add serializable to user model

--- src/main/java/com/example/library/models/User.java
+++ src/main/java/com/example/library/models/User.java
@@ -7,7 +7,8 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
-public class User  {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
 
     private Long enrollment;
