074c91720f61c3c05892cdcda23e6219fd09c337
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-06 12:39:40 -0300

    add User getters and setters

--- src/main/java/com/example/library/models/User.java
+++ src/main/java/com/example/library/models/User.java
@@ -30,5 +30,45 @@ public class User implements Serializable {
         this.phone = phone;
     }
 
+    public Long getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(Long enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
 
 }
