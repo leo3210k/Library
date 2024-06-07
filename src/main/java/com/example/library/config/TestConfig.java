3ce6556e8dd6038447fb097cf1a7d0748c95109f
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-07 12:44:55 -0300

    instantiate and persist datas on database

--- src/main/java/com/example/library/config/TestConfig.java
+++ src/main/java/com/example/library/config/TestConfig.java
@@ -18,6 +18,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "Rua Baiano", "86994778760");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "Rua Pinheiro", "86994778760");
 
+        userRepository.saveAll(Arrays.asList(u1, u2));
     }
 }
