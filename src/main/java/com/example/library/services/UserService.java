3b0923213a6384484b9016b97a172dccc21a6ba6
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-08 13:29:57 -0300

    add servide annotation on UserService

---
+++ src/main/java/com/example/library/services/UserService.java
@@ -0,0 +1,15 @@
+package com.example.library.services;
+
+import com.example.library.models.User;
+import com.example.library.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+
+}
