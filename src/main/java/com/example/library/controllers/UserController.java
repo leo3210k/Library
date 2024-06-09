1b5150084f6e1b97f0bc2dcc08f247275eed69ac
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-09 13:34:16 -0300

    create UserController

--- src/main/java/com/example/library/controllers/UserController.java
+++ src/main/java/com/example/library/controllers/UserController.java
@@ -1,4 +1,17 @@
 package com.example.library.controllers;
 
+import com.example.library.models.User;
+import com.example.library.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+
 public class UserController {
+
 }
