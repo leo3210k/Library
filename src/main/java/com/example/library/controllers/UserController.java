accf6fd60fbedca164e68b0086614a375398e31b
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-09 13:34:39 -0300

    add RestController and RequestMapping annotations to UserController

--- src/main/java/com/example/library/controllers/UserController.java
+++ src/main/java/com/example/library/controllers/UserController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
-
+@RestController
+@RequestMapping(value = "/users")
 public class UserController {
 
 }
