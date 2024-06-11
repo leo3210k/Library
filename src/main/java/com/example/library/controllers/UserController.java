bab464b7213fd55056babd609449bdeb656b0555
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-11 16:50:12 -0300

    add ResponseEntity to findAll method

--- src/main/java/com/example/library/controllers/UserController.java
+++ src/main/java/com/example/library/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.library.controllers;
 import com.example.library.models.User;
 import com.example.library.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,9 @@ public class UserController {
     private UserService service;
 
     @GetMapping
-    public List<User> findAll() {
-        return service.findAll();
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
     }
 
     @GetMapping(value = "/{enrollment}")
