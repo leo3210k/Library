2502cc5c0270c785e1ff3e669802cd13c66f3a64
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-12 20:38:52 -0300

    create book repository

--- src/main/java/com/example/library/repositories/BookRepository.java
+++ src/main/java/com/example/library/repositories/BookRepository.java
@@ -1,4 +1,7 @@
 package com.example.library.repositories;
 
-public interface BookRepository {
+import com.example.library.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
