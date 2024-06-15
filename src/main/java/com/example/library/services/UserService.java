909eadad390c5e565b5b8a99fec527a3bb735118
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-15 16:43:45 -0300

    create UserService interface

--- src/main/java/com/example/library/services/UserService.java
+++ src/main/java/com/example/library/services/UserService.java
@@ -1,4 +1,15 @@
 package com.example.library.services;
 
+import com.example.library.models.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> findAll();
+
+    User findByEnrollment(Long enrollment);
+
+    User insertUser(User user);
+
+    User update(Long enrollment, User user);
 }
