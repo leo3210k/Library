b79ea5b7d39e907bfdefa51c241680af24c299dc
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-16 16:44:08 -0300

    create LoanService interface

--- src/main/java/com/example/library/services/LoanService.java
+++ src/main/java/com/example/library/services/LoanService.java
@@ -1,4 +1,14 @@
 package com.example.library.services;
 
+import com.example.library.models.Loan;
+import com.example.library.models.Session;
+
+import java.util.List;
+
 public interface LoanService {
+    List<Loan> findAll();
+
+    Loan findByCode(Long code);
+
+    Loan insertLoan(Loan loan);
 }
