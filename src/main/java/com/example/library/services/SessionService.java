98c11b47fc94d848c418d51437221d33fdf84f01
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-16 16:44:36 -0300

    create SessionService interface

--- src/main/java/com/example/library/services/SessionService.java
+++ src/main/java/com/example/library/services/SessionService.java
@@ -1,4 +1,14 @@
 package com.example.library.services;
 
+import com.example.library.models.Session;
+import com.example.library.models.User;
+
+import java.util.List;
+
 public interface SessionService {
+    List<Session> findAll();
+
+    Session findByCode(Long code);
+
+    Session insertSession(Session session);
 }
