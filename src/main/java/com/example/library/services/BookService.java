53aee5a6781d40bb2fde43cff852829decdf4ab3
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-16 16:44:24 -0300

    create BookService interface

--- src/main/java/com/example/library/services/BookService.java
+++ src/main/java/com/example/library/services/BookService.java
@@ -1,4 +1,14 @@
 package com.example.library.services;
 
+import com.example.library.models.Book;
+import com.example.library.models.Loan;
+
+import java.util.List;
+
 public interface BookService {
+    List<Book> findAll();
+
+    Book findByCode(Long code);
+
+    Book insertBook(Book book);
 }
