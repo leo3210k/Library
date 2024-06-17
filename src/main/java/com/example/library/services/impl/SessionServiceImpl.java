ee0bd5028321fe83a252289933d40517577cc32d
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-06-17 16:50:19 -0300

    rename impl package

--- src/main/java/com/example/library/services/impl/SessionServiceImpl.java
+++ src/main/java/com/example/library/services/impl/SessionServiceImpl.java
@@ -1,7 +1,6 @@
-package com.example.library.services.Impl;
+package com.example.library.services.impl;
 
 import com.example.library.models.Session;
-import com.example.library.models.User;
 import com.example.library.repositories.SessionRepository;
 import com.example.library.services.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +20,5 @@ public class SessionServiceImpl implements SessionService {
     public Session findByCode(Long code) {
         Optional<Session> obj = repository.findById(code);
         return obj.get();
-    }
-
-    public Session insertSession(Session session) {
-        return repository.save(session);
     }
 }
